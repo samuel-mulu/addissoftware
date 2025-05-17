2ad9df7276e55527be5bd411716f9a86030dc4f5
samuel-mulu <Samuelmulu810@gmail.com>
2025-05-17 12:26:31 +0300

    Update vite.config.ts

--- songs-app/vite.config.ts
+++ songs-app/vite.config.ts
@@ -1,1 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, // Default Vite port
+  },
+  define: {
+    'process.env': process.env, // to access process.env in legacy libraries
+  },
+});
 
