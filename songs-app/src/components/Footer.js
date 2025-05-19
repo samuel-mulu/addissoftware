b399d0f99e7829a96d3657e39988c2eacb19588a
samuel-mulu <Samuelmulu810@gmail.com>
2025-05-19 12:34:08 +0300

    Update Footer.js

--- songs-app/src/components/Footer.js
+++ songs-app/src/components/Footer.js
@@ -1,11 +1,1 @@
-import React from 'react';
 
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>&copy; 2025 My Music App. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
