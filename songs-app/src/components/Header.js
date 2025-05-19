e288150395acd625841c194a460655f2fe3231b3
samuel-mulu <Samuelmulu810@gmail.com>
2025-05-19 12:34:24 +0300

    Update Header.js

--- songs-app/src/components/Header.js
+++ songs-app/src/components/Header.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const Header = () => {
-  return (
-    <header className="header">
-      <h1>My Music App</h1>
-      <p>Discover and manage your favorite songs!</p>
-    </header>
-  );
-};
-
-export default Header;
