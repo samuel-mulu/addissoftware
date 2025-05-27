2cc1ffc60b88fdbc57508960d06931d28560c3b3
samuel-mulu <Samuelmulu810@gmail.com>
2025-05-27 22:16:48 +0300

    Update store.ts

--- songs-app/src/redux/store.ts
+++ songs-app/src/redux/store.ts
@@ -1,12 +1,22 @@
-// store.ts
+// redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import songsReducer from './songsSlice';
+import { songsSaga } from './songsSaga';
+import statsReducer from './stats/statsSlice';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     songs: songsReducer,
+    stats: statsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(songsSaga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
