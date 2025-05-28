e73e13f608778c4672075f636db0539d48db3929
samuel-mulu <Samuelmulu810@gmail.com>
2025-05-28 08:58:31 +0300

    Update statsSaga.ts

--- songs-app/src/redux/statsSaga.ts
+++ songs-app/src/redux/statsSaga.ts
@@ -1,11 +1,15 @@
-// src/redux/stats/statsSaga.ts
+// redux/sagas/statsSaga.ts
 import { call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
-import { fetchStatsRequest, fetchStatsSuccess, fetchStatsFailure } from './statsSlice';
+import {
+  fetchStatsRequest,
+  fetchStatsSuccess,
+  fetchStatsFailure,
+} from './statsSlice';
 
 function* fetchStatsSaga(): any {
   try {
-    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
     const response = yield call(axios.get, `${API_BASE_URL}/stats/summary`);
     yield put(fetchStatsSuccess(response.data));
   } catch (error: any) {
@@ -13,6 +17,6 @@ function* fetchStatsSaga(): any {
   }
 }
 
-export default function* statsRootSaga() {
+export default function* watchStatsSaga() {
   yield takeLatest(fetchStatsRequest.type, fetchStatsSaga);
 }
